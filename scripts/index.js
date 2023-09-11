98cc7b6e9473ff09c02c2ab068f4845f34d515e4
Chiquin <99442194+leoziN77@users.noreply.github.com>
2023-09-11 13:49:16 -0300

    index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,25 @@ const navbar = document.querySelector('.navbar');
 const mobileNavbar = document.querySelector('.navbar__mobile');
 const button = document.querySelector('.burguer');
 
+function getVisitCount() {
+    let visitCount = localStorage.getItem("visitCount") || 0;
+    return parseInt(visitCount);
+}
+
+function updateVisitCount() {
+    const counterElement = document.getElementById("counter");
+    const visitCount = getVisitCount() + 1;
+    localStorage.setItem("visitCount", visitCount);
+    counterElement.textContent = visitCount;
+}
+
+window.addEventListener("load", updateVisitCount);
+
 button.addEventListener('click', function () {
-  mobileNavbar.classList.toggle('active');
+    mobileNavbar.classList.toggle('active');
 });
 
 window.addEventListener('scroll', function () {
-  if (this.window.pageYOffset > 0) return navbar.classList.add('active');
-  return navbar.classList.remove('active');
+    if (this.window.pageYOffset > 0) return navbar.classList.add('active');
+    return navbar.classList.remove('active');
 });
